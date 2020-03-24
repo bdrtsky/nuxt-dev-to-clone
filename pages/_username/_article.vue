b4676d991b2057a804840edc562656f986a0d5e8
Sergey Bedritsky <sergey.bedritsky@gmail.com>
2020-03-24 17:47:22 +0200

    change markup to place aside username profile on the bottom on narrow screen

--- pages/_username/_article.vue
+++ pages/_username/_article.vue
@@ -2,11 +2,11 @@
   <div class="page-wrapper">
     <div class="article-content-wrapper">
       <article-block class="article-block" />
-      <comments-block class="comments-block" />
+      <div class="aside-username-wrapper">
+        <aside-username-block class="aside-username-block" />
+      </div>
     </div>
-    <div class="aside-username-wrapper">
-      <aside-username-block class="aside-username-block" />
-    </div>
+    <comments-block class="comments-block" />
   </div>
 </template>
 
@@ -29,40 +29,45 @@ export default {
   max-width: $screen-xl;
   margin: auto;
   padding: 1rem;
-  display: flex;
 }
 
 .article-content-wrapper {
-  width: 100%;
-  max-width: 880px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   margin: auto;
-  @media (min-width: $screen-xl) {
-    margin-right: 1rem;
-    width: 66.66666%;
+  margin-bottom: 2rem;
+  @media (min-width: $screen-lg) {
+    align-items: normal;
+    flex-direction: row;
   }
   .article-block {
-    margin-bottom: 2rem;
-    @media (min-width: $screen-xl) {
+    width: 100%;
+    max-width: 880px;
+    @media (min-width: $screen-lg) {
+      margin-right: 1rem;
+      width: 66.66666%;
       margin-bottom: 2rem;
     }
   }
-  .comments-block {
-    @media (min-width: $screen-xl) {
-      margin: 0.5rem;
+  .aside-username-wrapper {
+    max-width: 880px;
+    width: 100%;
+    @media (min-width: $screen-lg) {
+      display: block;
+      width: 33.33333%;
+    }
+    position: relative;
+    .aside-username-block {
+      position: sticky;
+      top: 1rem;
+      // margin: 0.5rem;
     }
   }
 }
 
-.aside-username-wrapper {
-  display: none;
+.comments-block {
   @media (min-width: $screen-xl) {
-    display: block;
-    width: 33.33333%;
-  }
-  position: relative;
-  .aside-username-block {
-    position: sticky;
-    top: 1rem;
     margin: 0.5rem;
   }
 }
